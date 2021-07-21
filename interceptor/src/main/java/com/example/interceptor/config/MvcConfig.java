7445b7a21644e2f393c9b51b2ca49753d58bd51b
parkjekyll <parkjekyll@naver.com>
2021-07-21 23:18:37 +0900

    ch6. filter, interceptor, async(210721)

---
+++ interceptor/src/main/java/com/example/interceptor/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.interceptor.config;
+
+import com.example.interceptor.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class MvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/api/private/*");
+        //addPathPatterns 해당 경로 검사하겠다.
+        //exclude -> 얘는 빼고 검사하겠다
+    }
+}
