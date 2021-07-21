834b3a206d4273425c6f8300b27681b5311e648b
parkjekyll <parkjekyll@naver.com>
2021-07-21 18:47:57 +0900

    ch.6 validation(210721)

---
+++ validation/src/main/java/com/example/validation/dto/User.java
@@ -0,0 +1,56 @@
+package com.example.validation.dto;
+
+import com.example.validation.annotation.YearMonth;
+import org.springframework.boot.autoconfigure.web.format.DateTimeFormatters;
+import org.springframework.format.datetime.standard.DateTimeFormatterFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class User {
+
+    @NotBlank
+    private String name;
+
+    @Max(value = 90)
+    private int age;
+
+    @Valid // Valid 꼭 붙여야 검사가 된다~
+    private List<Car> cars;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", cars=" + cars +
+                '}';
+    }
+}
