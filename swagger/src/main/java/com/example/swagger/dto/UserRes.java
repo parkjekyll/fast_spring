bdca6b3ad8c52fed1cc3ac953f12ac46faba2354
parkjekyll <parkjekyll@naver.com>
2021-07-24 17:09:47 +0900

    ch9.swagger(210724)

---
+++ swagger/src/main/java/com/example/swagger/dto/UserRes.java
@@ -0,0 +1,16 @@
+package com.example.swagger.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRes {
+    @ApiModelProperty(value="사용자의 이름", example = "pinggu", required = true)
+    private String name;
+    @ApiModelProperty(value="사용자의 나이", example = "2", required = true)
+    private int age;
+}
