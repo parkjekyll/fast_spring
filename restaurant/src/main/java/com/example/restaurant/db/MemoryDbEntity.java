2808cd149a63499366f35e8e134ba8c964a53a18
parkjekyll <parkjekyll@naver.com>
2021-07-24 22:07:15 +0900

    ch10.네이버 api 활용해서 맛집 리스트 만들기(210724완강)

---
+++ restaurant/src/main/java/com/example/restaurant/db/MemoryDbEntity.java
@@ -0,0 +1,12 @@
+package com.example.restaurant.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemoryDbEntity {
+    protected Integer index;
+}
---
+++ restaurant/src/main/java/com/example/restaurant/wishlist/entity/WishListEntity.java
@@ -0,0 +1,23 @@
+package com.example.restaurant.wishlist.entity;
+
+import com.example.restaurant.db.MemoryDbEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishListEntity extends MemoryDbEntity {
+    private String title;
+    private String category;
+    private String address;
+    private String roadAddress;
+    private String homePageLink;
+    private String imageLink;
+    private boolean isVisit;
+    private int visitCount;
+    private LocalDateTime lastVisitDate;
+}
